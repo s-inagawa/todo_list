51f7a4ef8a0508ac96048271be13259236ed5496
稲川新太郎 <s-inagawa@inagawashintarou-no-MacBook-Air.local>
2014-02-22 14:51:49 +0900

    add datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,6 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-datepicker
+//= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.es.js
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.fr.js
